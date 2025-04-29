8ec7c8684d797cb0d15abc6bdc411a02e31905f9
Jeb Bailey <jibailey@caltech.edu>
2025-04-29 08:40:52 -0700

    It builds Jim. It's probably dead, Jim.
    Removed redundant RS485 setup and Modbus client initialization from `maiman.cpp` to streamline the code. Updated UART settings in `main.cpp` for consistency and replaced unused UART definitions with meaningful ones.

--- programs/tib-pico2/main.cpp
+++ programs/tib-pico2/main.cpp
@@ -31,19 +31,19 @@
 #define YJ_PHOTODIODE_CHANNEL 0
 #define HK_PHOTODIODE_CHANNEL 1
 
-#define UART0_TX_PIN 0
-#define UART0_RX_PIN 1
-#define UART1_TX_PIN 4
-#define UART1_RX_PIN 5
-#define MB_UART_ID uart1
-#define MB_UART_BAUDRATE 115200
+// #define UART0_TX_PIN 0
+// #define UART0_RX_PIN 1
+// #define UART1_TX_PIN 4
+// #define UART1_RX_PIN 5
+// #define MB_UART_ID uart1
+// #define MB_UART_BAUDRATE 115200
 
 
 // Define UART pins and settings
 // We are using pins 0 and 1, but see the GPIO function select table in the
 // datasheet for information on which other pins can be used.
 #define UART_ID uart0
-#define BAUD_RATE 19200
+#define BAUD_RATE 115200
 #define DATA_BITS 8
 #define STOP_BITS 1
 #define PARITY UART_PARITY_ODD
